a84158d608222a08922a069e22cb694237d41160
Sarah Scott <s.j.scott84@gmail.com>
2017-08-24 10:45:49 +1200

    Fix all bugs and add comments

--- code_sample_totara.php
+++ code_sample_totara.php
@@ -5,6 +5,7 @@
   </head>
   <body>
     <?php
+
       //Helper function to log messages to chrome console
       function debug_to_console( $data ) {
         $output = $data;
@@ -17,13 +18,18 @@
       //take section - remove white space at front and back
       function tidyString($string){
         $tidyString = trim($string);
-        return $tidyString."\n";
+        if (strlen($tidyString) > 0) {
+          return $tidyString."\n";
+        } else {
+          return $tidyString;
+        }
       }
 
+      //Create the new string and update variables needed to keep track of location
       function createString($string, $newString, $split, $length, $growLength, $addLength){
+        $nextChar = substr($string, $length, 1);
         $nextString = tidyString(substr($string, $split, $length));
         $newString = $newString.$nextString;
-        //error_log($newString);
         echo $newString."<br>";
         $split = $split + $addLength;
         $newLength = $growLength + $addLength;
@@ -39,6 +45,11 @@
           //If string less then length simply return string
           if (strlen($string) <= $length) {
             return $string;
+          //This check ensures there are no PHP Notice:  Uninitialized string offset warnings
+          //by simply adding the last characters in $string to $newString
+          } elseif ($growLength > strlen($string)) {
+            list($newString, $startSplit, $growLength) = createString($string, $newString,
+              $startSplit, $length, $growLength, $length);
           //If character next to one that is to be split is space, split there
           } elseif ($string[$growLength + 1] === " ") {
             list($newString, $startSplit, $growLength) = createString($string, $newString,
@@ -47,28 +58,58 @@
           } elseif ($string[$growLength] === " ") {
             list($newString, $startSplit, $growLength) = createString($string, $newString,
               $startSplit, $length, $growLength, $length - 1);
-          //Find the nearest space and split there
+          //Find the nearest space to the left and split there
           } else {
             for ($count = 1; $count <= $length; $count++) {
-              if ($count === $length) {
-                list($newString, $startSplit, $growLength) = createString($string, $newString,
-                  $startSplit, $length, $growLength, $length);
-              } elseif ($string[$growLength - $count] === " ") {
+              if ($string[$growLength - $count] === " " && $count !== $length) {
                 $tempLength = $length - $count;
                 list($newString, $startSplit, $growLength) = createString($string, $newString,
                   $startSplit, $tempLength, $growLength, $tempLength);
                 break;
+              } elseif ($count === $length ) {
+                list($newString, $startSplit, $growLength) = createString($string, $newString,
+                  $startSplit, $length, $growLength, $length);
               }
             }
           }
-          if ($growLength - $length >= strlen($string)){
+          //Exit while loop once $growLength is greater the $string - also check if last character in
+          //$newString is "\n" and remove if it is.
+          if ($growLength - $length >= strlen($string)) {
             $complete = True;
+            if($newString[strlen($newString) - 1] === PHP_EOL) {
+              $newString = substr($newString, 0, strlen($newString) - 1);
+            }
           }
         }
         return $newString;
       }
+
+      //Tests to ensure string is being created correctly
+
       $test = wrap("This is a string that I am testing.", 7);
       error_log($test);
+      if($test === "This is\na\nstring\nthat I\nam\ntesting\n."){
+        error_log("True");
+      } else {
+        error_log("False");
+      }
+
+      /*$test = wrap("This is a string that I am testing.", 3);
+      error_log($test);
+      if($test === "Thi\ns\nis\na\nstr\ning\ntha\nt I\nam\ntes\ntin\ng."){
+        error_log("True");
+      } else {
+        error_log("False");
+      }*/
+
+      /*$test = wrap("This is a string that I am testing.", 15);
+      error_log($test);
+      if($test === "This is a\nstring that I\nam testing."){
+        error_log("True");
+      } else {
+        error_log("False");
+      }*/
+      
     ?>
     </body>
 </html>
