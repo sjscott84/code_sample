ba55565da4693df53d699ceb8b26404673219600
Sarah Scott <s.j.scott84@gmail.com>
2017-08-23 12:17:01 +1200

    Remove white spaces

--- code_sample_totara.php
+++ code_sample_totara.php
@@ -5,51 +5,64 @@
   </head>
   <body>
     <?php
+      //Helper function to log messages to chrome console
       function debug_to_console( $data ) {
         $output = $data;
         if ( is_array( $output ) ) {
           $output = implode( ',', $output);
         }
-          echo "<script>console.log( 'Debug Objects: " . $output . "' );</script>";
+        echo "<script>console.log( 'Debug Objects: " . $output . "' );</script>";
+      }
+
+      //take section - remove white space at front and back
+      function tidyString($string){
+        $tidyString = trim($string);
+        return $tidyString."\n";
       }
 
       function wrap ($string, $length) {
         $newString = "";
         $growLength = $length - 1;
         $startSplit = 0;
-        while (strlen($newString) < strlen($string)){
+        while ( strlen($string) + $length >= strlen($newString) ){
           if (strlen($string) <= $length) {
             return $string;
           } elseif ($string[$growLength + 1] === " ") {
-              debug_to_console("Next char space");
-              $newString = $newString.substr($string, $startSplit, $length)."\n";
-              echo $newString.",<br>";
-              $startSplit = $startSplit + $length + 1;
-              $growLength = $growLength + $length + 1;
+            $nextString = tidyString(substr($string, $startSplit, $length));
+            $newString = $newString.$nextString;
+            error_log($newString);
+            echo $newString."<br>";
+            $startSplit = $startSplit + $length + 1;
+            $growLength = $growLength + $length + 1;
           } elseif ($string[$growLength] === " ") {
-              debug_to_console("This char space");
-              $newString = $newString.substr($string, $startSplit, $length - 1)."\n";
-              echo $newString.",<br>";
-              $startSplit = $startSplit + $length - 1;
-              $growLength = $growLength + $length - 1;
+            $nextString = tidyString(substr($string, $startSplit, $length));
+            $newString = $newString.$nextString;
+            echo $newString."<br>";
+            error_log($newString);
+            $startSplit = $startSplit + $length - 1;
+            $growLength = $growLength + $length - 1;
           } else {
-              for ($count = 1; $count <= $length; $count++) {
-                if ($count === $length) {
-                  $newString = $newString.substr($string, $startSplit, $length)."\n";
-                } elseif ($string[$growLength - $count] === " ") {
-                  $newString = $newString.substr($string, $startSplit, $length - $count)."\n";
-                  break;
-                }
+            for ($count = 1; $count <= $length; $count++) {
+              if ($count === $length) {
+                $nextString = tidyString(substr($string, $startSplit, $length));
+                $newString = $newString.$nextString;
+                $startSplit = $startSplit + $length;
+                $growLength = $growLength + $length;
+              } elseif ($string[$growLength - $count] === " ") {
+                $nextString = tidyString(substr($string, $startSplit, $length - $count));
+                $newString = $newString.$nextString;
+                $startSplit = $startSplit + $length - $count;
+                $growLength = $growLength + $length - $count;
+                break;
               }
-              echo $newString.",<br>";
-              $startSplit = $startSplit + $length;
-              $growLength = $growLength + $length;
+            }
+            echo $newString."<br>";
+            error_log($newString);
           }
         }
         return $newString;
       }
-      
-      wrap("This is a string that I am testing.", 4);
+      wrap("This is a string that I am testing.", 3);
     ?>
     </body>
 </html>
